0253a5ba8cd1f4b1e7c9e1d3bae4f575c0a4d507
matheus proença <matheusproenca2020@hotmail.com>
2023-12-07 17:58:15 -0300

    Finalizando Req08,09

--- src/recipes-api/Controllers/UserController.cs
+++ src/recipes-api/Controllers/UserController.cs
@@ -45,13 +45,29 @@ public class UserController : ControllerBase
     [HttpPut("{email}")]
     public IActionResult Update(string email, [FromBody] User user)
     {
-        throw new NotImplementedException();
+        var findUser = _service.UserExists(user.Email);
+        if (findUser == true)
+        {
+            _service.UpdateUser(user);
+            return Ok(user);
+        }
+        else if (email == null || email != user.Email)
+        {
+            return BadRequest();
+        }
+        return NotFound();
     }
 
     // 9 - Sua aplicação deve ter o endpoint DEL /user
     [HttpDelete("{email}")]
     public IActionResult Delete(string email)
     {
-        throw new NotImplementedException();
+        var findUser = _service.UserExists(email);
+        if (findUser == true)
+        {
+            _service.DeleteUser(email);
+            return NoContent();
+        }
+        return NotFound();
     }
 }
