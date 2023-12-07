0dd9b23b01751476c37b8d50a22d99d71e384a5c
matheus proença <matheusproenca2020@hotmail.com>
2023-12-07 17:45:18 -0300

    Finalizando Req06,07

--- src/recipes-api/Controllers/UserController.cs
+++ src/recipes-api/Controllers/UserController.cs
@@ -12,31 +12,38 @@ namespace recipes_api.Controllers;
 [ApiController]
 [Route("user")]
 public class UserController : ControllerBase
-{    
+{
     public readonly IUserService _service;
-    
+
     public UserController(IUserService service)
     {
-        this._service = service;        
+        this._service = service;
     }
 
     // 6 - Sua aplicação deve ter o endpoint GET /user/:email
     [HttpGet("{email}", Name = "GetUser")]
     public IActionResult Get(string email)
-    {                
-        throw new NotImplementedException();
+    {
+        var findUser = _service.UserExists(email);
+        if (findUser == true)
+        {
+            var getUser = _service.GetUser(email);
+            return Ok(getUser);
+        }
+        return NotFound();
     }
 
     // 7 - Sua aplicação deve ter o endpoint POST /user
     [HttpPost]
-    public IActionResult Create([FromBody]User user)
+    public IActionResult Create([FromBody] User user)
     {
-        throw new NotImplementedException();
+        _service.AddUser(user);
+        return Created("", user);
     }
 
     // "8 - Sua aplicação deve ter o endpoint PUT /user
     [HttpPut("{email}")]
-    public IActionResult Update(string email, [FromBody]User user)
+    public IActionResult Update(string email, [FromBody] User user)
     {
         throw new NotImplementedException();
     }
@@ -46,5 +53,5 @@ public class UserController : ControllerBase
     public IActionResult Delete(string email)
     {
         throw new NotImplementedException();
-    } 
+    }
 }
